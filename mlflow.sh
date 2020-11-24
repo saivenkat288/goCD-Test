b84799096e808b18c48c77fb8423ddbe76775070
Sai venkat <36558513+saivenkat288@users.noreply.github.com>
2020-11-24 20:36:02 +0530

    Update mlflow.sh

--- mlflow.sh
+++ mlflow.sh
@@ -1,12 +1,10 @@
 #Initializing two variables 
-a=20 
-b=20 
+a="training" 
+b="training"
   
 if [ $a == $b ] 
 then 
-    #If they are equal then print this 
-    echo "a is equal to b"
-    #/home/ubuntu/anaconda3/bin/mlflow run .
+    /home/ubuntu/anaconda3/bin/mlflow run .
 else
     #else print this 
     echo "a is not equal to b"
